906f9c420cd2a6d04707caa1d715f820796295b3
Thomas <45773547+Sylimaris@users.noreply.github.com>
2019-03-05 21:02:19 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,61 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+<link href="cssIndex.css" rel="stylesheet" media="all" type="text/css"> 
+
+<title>Boucherie Order</title>
+<meta charset="UTF-8">
+</head>
+
+
+<body>
+<img class="displayed" src="/Pictures/Viande.png" alt="Image d'accueil" style="object-position: center top;">
+
+<br><br><br>
+
+<h2>Vous êtes client : Commandez vos produits frais avec beaucoup d'options!</h2>
+<br>
+<h2>Vendez vos lots aux consommateur au meilleur prix, en évitant des intermédiaires coûteux!</h2>
+<br><br>
+
+<center>
+<div>
+<form>
+  Nom d'utilisateur:
+    <br>
+  <input type="text" name="username" value="">
+    <br>
+    <br>
+  Mot de passe:
+    <br>
+  <input type="password" name="password" value="">
+    <br><br>
+</form> 
+
+  <button>Se connecter</button>
+    <br><br><br>
+  <button>S'inscrire</button>
+    <br><br><br>
+  <button>Mot de passe oublié </button>
+    <br><br><br>
+</div>
+</center>
+
+
+
+ <br><br><br><br><br><br>
+
+ <hr>
+ <p class="badpage">
+Projet annuel "Boucherie Order" en développement par Pierre-baptiste COUGNENC et Thomas LEONARDON
+ </p>
+</body>
+
+</html>
+
+<?php
+?>
